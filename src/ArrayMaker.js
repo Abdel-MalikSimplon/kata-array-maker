4edb74f43ebf389c6c1bda4bdcf2287914cc16f8
simplon-mars-2-Abdel-Malik <abdelmalik.bouali@yahoo.com>
2015-11-27 11:42:25 +0100

    ajoute de mon code

--- src/ArrayMaker.js
+++ src/ArrayMaker.js
@@ -4,4 +4,15 @@ function arrayMaker(max) {
   } else if(max === 0) {
     return [];
   }
+  function arrayMaker(max) {
+    var i;
+    while (i < max) {
+        var tableau = [i];
+        return tableau;
+        i+=1;
+    }
+}
+
+console.log(arrayMaker(10));
+
 }
